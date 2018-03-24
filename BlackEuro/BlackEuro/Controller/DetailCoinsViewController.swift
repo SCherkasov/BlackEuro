57e73c1eb7b94aefb130931c9dc9cf049ed36a1e
Stasik <scherkasov8@gmail.com>
2018-03-24 20:22:10 +0200

    added DetailCoinsViewController

---
+++ BlackEuro/BlackEuro/Controller/DetailCoinsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailCoinsViewController.swift
+//  BlackEuro
+//
+//  Created by Stanislav Cherkasov on 24.03.18.
+//  Copyright © 2018 Stanislav Cherkasov. All rights reserved.
+//
+
+import UIKit
+
+class DetailCoinsViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
